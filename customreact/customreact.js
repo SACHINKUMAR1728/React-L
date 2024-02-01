777aaa6e1c2b261f79c23f788805d3aacd7baeb1
SACHINKUMAR1728 <sachinsubh880@gmail.com>
2024-02-01 22:50:18 +0530

    improved

--- customreact/customreact.js
+++ customreact/customreact.js
@@ -1,8 +1,8 @@
 const reactElement = {
-    type = "a",
-    props = {
-        href ="https://www.google.com",
-        target = "_blank";
+    type : "a",
+    props : {
+        href :"https://www.google.com",
+        target : "_blank"
     },
     children: 'click me to visit google'
 }
